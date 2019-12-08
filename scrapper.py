6e2132a9e4e02e112a299418f5a5a62cac0ab7fc
Beka Chkhaidze <56247764+chkhaidzebeka@users.noreply.github.com>
2019-12-08 12:46:34 +0400

    updated

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8
 
-# pip3 install telethon
+# pip3 install -r requirements.txt
 
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
@@ -9,8 +9,9 @@ from telethon.tl.types import InputPeerEmpty
 import csv
 
 api_id = 11111111       #CHANGE IT WITH YOUR ID
-api_hash = 'xxxxxxxxxxxxxxxxx'       # CHANGE IT WITH YOUR HASH
-phone = '+xxxxxxxxx'
+api_hash = '1111111111111'       # CHANGE IT WITH YOUR HASH
+phone = '+11111111111111'     # change phone number
+target_group='test'
 client = TelegramClient(phone, api_id, api_hash)
 
 client.connect()
@@ -18,7 +19,7 @@ if not client.is_user_authorized():
     client.send_code_request(phone)
     client.sign_in(phone, input('enter reveived code: '))
 
-
+chats=[]
 last_date = None
 chunk_size = 200
 
@@ -29,33 +30,50 @@ result = client(GetDialogsRequest(
              limit=chunk_size,
              hash = 0
          ))
+chats.extend(result.chats)
 
-target_group='group name'       # change it with your grup nick
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
 
-print('Scrapping Members...')
+filename=f'{target_group}.csv'
+print(f'Scrapping Members from {target_group}')
 all_participants = []
 all_participants = client.get_participants(target_group, aggressive=True)
 
-print('Saving In members.csv...')
-with open('members.csv', 'w') as f:
+
+# user.id   user.access_hash user.username user.first_name user.last_name
+print(f'Saving In {filename}...')
+with open(filename, 'w') as f:
     writer = csv.writer(f,delimiter=",",lineterminator="\n")
-    writer.writerow(['username','first name','last name'])
     for user in all_participants:
-        if user.username:
-            username= user.username
+        if user.id:
+            uid=user.id
         else:
-            username="ERROR"
+            uid=""
+
+        if user.access_hash:
+            uaccess_hash=user.access_hash
+        else:
+            uaccess_hash=""
+
+        if user.username:
+            username=user.username
+        else:
+            username=""
+
         if user.first_name:
             first_name=user.first_name
         else:
-            first_name="ERROR"
+            first_name=""
+
         if user.last_name:
-            last_name=user.last_name
+            last_name= user.last_name
         else:
-            last_name="ERROR"
-        if user.id:
-            user_id=user.id
-        else:
-            user_id="ERROR"
-        print(f'{username},{first_name},{last_name}',file=f)
+            last_name= ""
+        print(f'{uid},{username},{first_name},{last_name},{uaccess_hash}',file=f)
+
 print('Members scraped successfully.')
